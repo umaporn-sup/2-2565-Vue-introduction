dc7b0cbb738305a957e4ac33afec66d908eab33f
umaporn-sup <umaporn@sit.kmutt.ac.th>
2023-01-18 16:54:16 +0700

    [add] counter-project

---
+++ counter-project/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  theme: {
+    extend: {}
+  },
+  plugins: [require('daisyui')],
+  daisyui: {
+    styled: true,
+    themes: ['cupcake', 'dark', 'cmyk'],
+
+    base: true,
+    utils: true,
+    logs: true,
+    rtl: false,
+    prefix: '',
+    darkTheme: 'dark'
+  }
+}
